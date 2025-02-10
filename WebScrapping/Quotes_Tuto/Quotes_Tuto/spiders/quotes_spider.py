367c42ab93337fa66257385d51f935f89a60343f
Dipak Desai <123081533+dipakdesai1999@users.noreply.github.com>
2025-02-10 11:16:04 +0530

    files added

---
+++ WebScrapping/Quotes_Tuto/Quotes_Tuto/spiders/quotes_spider.py
@@ -0,0 +1,12 @@
+import scrapy
+
+
+class QuoteSpider(scrapy.Spider):
+    name = 'quotes'
+    start_urls = [
+        'https://quotes.toscrape.com/'
+    ]
+
+    def parse(self, response):
+        title = response.css('title').extract()
+        yield {'Title Name':title}
